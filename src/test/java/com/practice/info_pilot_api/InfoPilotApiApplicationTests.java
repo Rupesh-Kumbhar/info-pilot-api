ca8c052686dfdd98701a9868ad481bb31441b0c8
Kumbhar <2235367@cognizant.com>
2026-07-18 21:52:03 +0530

    Initial commit

---
+++ src/test/java/com/practice/info_pilot_api/InfoPilotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.info_pilot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoPilotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
