70770b295f53ea17e9861f874819a26cbb6413ce
Kumbhar <2235367@cognizant.com>
2026-07-22 19:10:32 +0530

    Added MCV layers for Documents

---
+++ src/main/java/com/practice/info_pilot_api/service/impl/DocumentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.practice.info_pilot_api.service.impl;
+
+import com.practice.info_pilot_api.service.DocumentService;
+import org.springframework.stereotype.Service;
+
+/*
+Upload File
+Extract Text
+Call Gemini
+ */
+
+
+@Service
+public class DocumentServiceImpl implements DocumentService {
+
+}
