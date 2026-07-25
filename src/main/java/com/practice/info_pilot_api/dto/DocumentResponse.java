437b835270370c95eeb78ee092b6b38ab5f190ca
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-07-25 16:31:31 +0530

    Exception handling and Delete document implemented

--- src/main/java/com/practice/info_pilot_api/dto/DocumentResponse.java
+++ src/main/java/com/practice/info_pilot_api/dto/DocumentResponse.java
@@ -1,4 +1,20 @@
 package com.practice.info_pilot_api.dto;
 
 public class DocumentResponse {
+
+    private Long id;
+    private String fileName;
+
+    public DocumentResponse(Long id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
 }
--- src/main/java/com/practice/info_pilot_api/service/DocumentService.java
+++ src/main/java/com/practice/info_pilot_api/service/DocumentService.java
@@ -1,7 +1,13 @@
 package com.practice.info_pilot_api.service;
+import com.practice.info_pilot_api.dto.DocumentResponse;
 import com.practice.info_pilot_api.dto.UploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface DocumentService {
     UploadResponse uploadDocument(MultipartFile file);
+    List<DocumentResponse> getAllDocuments();
+
+    void deleteDocumentById(Long id);
 }
