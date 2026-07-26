0cb1b17bca19f1f21de9224e54d5cf45d18e46cb
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-07-26 15:14:36 +0530

    Added chat foundation / architecture and Gemini integration foundation

--- src/main/java/com/practice/info_pilot_api/controller/ChatController.java
+++ src/main/java/com/practice/info_pilot_api/controller/ChatController.java
@@ -1,4 +1,35 @@
 package com.practice.info_pilot_api.controller;
 
+import com.practice.info_pilot_api.dto.ChatRequest;
+import com.practice.info_pilot_api.dto.ChatResponse;
+import com.practice.info_pilot_api.service.ChatService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/chat")
 public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(
+            ChatService chatService) {
+
+        this.chatService = chatService;
+    }
+
+    @PostMapping("/ask")
+    public ChatResponse askQuestion(
+            @RequestBody
+            ChatRequest request) {
+
+        String answer =
+                chatService
+                        .askQuestion(
+                                request.getQuestion()
+                        );
+
+        return new ChatResponse(
+                answer
+        );
+    }
 }
--- src/main/java/com/practice/info_pilot_api/dto/ChatRequest.java
+++ src/main/java/com/practice/info_pilot_api/dto/ChatRequest.java
@@ -1,4 +1,13 @@
 package com.practice.info_pilot_api.dto;
 
 public class ChatRequest {
+
+    private String question;
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
